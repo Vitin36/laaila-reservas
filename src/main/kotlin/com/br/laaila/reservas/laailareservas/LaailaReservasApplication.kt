c32e78d51a5fc65a1fddd2e958e3b6d60a3e17e5
Vitin36 <vitin_36@hotmail.com>
2020-07-22 13:22:49 -0300

    first commit

---
+++ src/main/kotlin/com/br/laaila/reservas/laailareservas/LaailaReservasApplication.kt
@@ -0,0 +1,11 @@
+package com.br.laaila.reservas.laailareservas
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LaailaReservasApplication
+
+fun main(args: Array<String>) {
+    runApplication<LaailaReservasApplication>(*args)
+}
---
+++ src/test/kotlin/com/br/laaila/reservas/laailareservas/LaailaReservasApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.br.laaila.reservas.laailareservas
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LaailaReservasApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
